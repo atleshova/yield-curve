235579177a26af29d1cdd1f7b4acdcf6b8b5e7cf
atleshova <46876713+atleshova@users.noreply.github.com>
2026-05-10 13:58:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,13 +24,12 @@ def read_uploaded_file(uploaded_file):
 
     if filename.endswith(".CSV"):
         df = pd.read_csv(uploaded_file)
-else:
-    xls = pd.ExcelFile(uploaded_file)
-
-    if "Daily" in xls.sheet_names:
-        df = pd.read_excel(uploaded_file, sheet_name="Daily")
     else:
-        df = pd.read_excel(uploaded_file)
+        xls = pd.ExcelFile(uploaded_file)
+        if "Daily" in xls.sheet_names:
+            df = pd.read_excel(uploaded_file, sheet_name="Daily")
+        else:
+            df = pd.read_excel(uploaded_file)
 
     df.columns = [str(c).strip() for c in df.columns]
 
